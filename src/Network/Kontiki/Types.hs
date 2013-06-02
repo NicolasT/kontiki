a979326909403e1b161c9e455980670cffb8da58
Nicolas Trangez <ikke@nicolast.be>
2013-06-02 22:29:37 +0200

    Start working on documentation

--- src/Network/Kontiki/Types.hs
+++ src/Network/Kontiki/Types.hs
@@ -56,8 +56,10 @@ import Test.QuickCheck (Arbitrary(arbitrary, shrink), Gen, elements, listOf, lis
 
 import GHC.Generics
 
+-- | Identifier of a node.
 type NodeId = ByteString
 
+-- | Index of log entries.
 newtype Index = Index { unIndex :: Word64 }
   deriving (Show, Eq, Ord)
 
@@ -69,15 +71,19 @@ instance Arbitrary Index where
     arbitrary = Index `fmap` arbitrary
     shrink = map Index . shrink . unIndex
 
+-- | Starting `Index'.
 index0 :: Index
 index0 = Index 0
 
+-- | Calculate the successor of a given `Index'.
 succIndex :: Index -> Index
 succIndex (Index i) = Index (succ i)
 
+-- | Calculate the predecessor of a given `Index'.
 prevIndex :: Index -> Index
 prevIndex (Index i) = Index (if i == 0 then 0 else i - 1)
 
+-- | Representation of a term.
 newtype Term = Term Word64
   deriving (Show, Eq, Ord)
 
@@ -89,15 +95,18 @@ instance Arbitrary Term where
     arbitrary = Term `fmap` arbitrary
     shrink (Term t) = map Term $ shrink t
 
+-- | Starting `Term'.
 term0 :: Term
 term0 = Term 0
 
+-- | Calculate the successor of a given `Term'.
 succTerm :: Term -> Term
 succTerm (Term i) = Term (succ i)
 
-data Entry a = Entry { eIndex :: Index
-                     , eTerm :: Term
-                     , eValue :: a
+-- | Entry as stored in a log.
+data Entry a = Entry { eIndex :: Index  -- ^ `Index' of the entry
+                     , eTerm :: Term    -- ^ `Term' of the entry
+                     , eValue :: a      -- ^ Value contained in the entry
                      }
   deriving (Show, Eq)
 
@@ -113,10 +122,11 @@ instance Arbitrary a => Arbitrary (Entry a) where
         v <- shrink eValue
         return $ Entry i t v
 
-data Config = Config { _configNodeId :: NodeId
-                     , _configNodes :: Set NodeId
-                     , _configElectionTimeout :: Int
-                     , _configHeartbeatTimeout :: Int
+-- | Configuration of a cluster.
+data Config = Config { _configNodeId :: NodeId        -- ^ `NodeId' of the running node
+                     , _configNodes :: Set NodeId     -- ^ Set of `NodeId's of all nodes in the cluster
+                     , _configElectionTimeout :: Int  -- ^ Election timeout
+                     , _configHeartbeatTimeout :: Int -- ^ Heartbeat timeout
                      }
   deriving (Show, Eq)
 makeLenses ''Config
@@ -129,11 +139,12 @@ instance Arbitrary Config where
         h <- abs `fmap` arbitrary
         return $ Config n (Set.fromList (n : ns)) e h
 
-
+-- | Generate an arbitrary `ByteString'.
 arbitraryBS :: Gen ByteString
 arbitraryBS = BS8.pack `fmap` listOf1 arbitrary
 
 
+-- | State kept when in `Follower' mode.
 data FollowerState = FollowerState { _fCurrentTerm :: Term
                                    , _fVotedFor :: Maybe NodeId
                                    }
@@ -148,6 +159,7 @@ instance Arbitrary FollowerState where
         FollowerState <$> arbitrary <*> elements [Nothing, Just n]
 
 
+-- | State kept when in `Candidate' mode.
 data CandidateState = CandidateState { _cCurrentTerm :: Term
                                      , _cVotes :: Set NodeId
                                      }
@@ -162,6 +174,7 @@ instance Arbitrary CandidateState where
         CandidateState <$> arbitrary <*> pure v
 
 
+-- | State kept when in `Leader' mode.
 data LeaderState = LeaderState { _lCurrentTerm :: Term
                                , _lNextIndex :: Map NodeId Index
                                , _lLastIndex :: Map NodeId Index
@@ -177,15 +190,18 @@ instance Arbitrary LeaderState where
                             <*> (Map.fromList `fmap` (listOf1 $ (,) <$> arbitraryBS <*> arbitrary))
 
 
+-- | Running modes.
 data Mode = MFollower
           | MCandidate
           | MLeader
   deriving (Show, Eq)
 
+-- * Utility type aliases for all running `Mode's.
 type Follower = State 'MFollower
 type Candidate = State 'MCandidate
 type Leader = State 'MLeader
 
+-- | State of a node.
 data State (s :: Mode) where
     Follower :: FollowerState -> Follower
     Candidate :: CandidateState -> Candidate
@@ -219,6 +235,9 @@ instance Arbitrary Leader where
     shrink (Leader s) = map Leader $ shrink s
 
 
+-- | Existential wrapper for `State'.
+--
+-- This is what's exposed to public API.
 data SomeState where
     WrapState :: State s -> SomeState
 
@@ -262,11 +281,15 @@ instance Arbitrary SomeState where
         WrapState (s'@Candidate{}) -> map WrapState $ shrink s'
         WrapState (s'@Leader{}) -> map WrapState $ shrink s'
 
+-- | Type family mapping a `State' to the corresponding internal state
+-- type.
 type family InternalState (s :: *) :: *
 type instance InternalState Follower = FollowerState
 type instance InternalState Candidate = CandidateState
 type instance InternalState Leader = LeaderState
 
+-- | A `Wrapable' is an internal state which can be wrapped into a `State'
+-- (and as such into a `SomeState').
 class Wrapable s where
     wrap :: s -> SomeState
 
@@ -280,14 +303,17 @@ instance Wrapable LeaderState where
     wrap s = WrapState $ Leader s
 
 
+-- | Representation of an election timeout.
 data ElectionTimeout = ElectionTimeout
   deriving (Show, Eq)
+-- | Representation of a heartbeat timeout.
 data HeartbeatTimeout = HeartbeatTimeout
   deriving (Show, Eq)
 
-data Event a = EMessage NodeId (Message a)
-             | EElectionTimeout
-             | EHeartbeatTimeout
+-- | Representation of some incoming event.
+data Event a = EMessage NodeId (Message a) -- ^ Incoming message from some node
+             | EElectionTimeout            -- ^ Election timeout timer fired
+             | EHeartbeatTimeout           -- ^ Heartbeat timeout timer fired
   deriving (Show, Eq)
 
 instance Arbitrary a => Arbitrary (Event a) where
@@ -296,13 +322,15 @@ instance Arbitrary a => Arbitrary (Event a) where
         m <- arbitrary
         elements [EMessage n m, EElectionTimeout, EHeartbeatTimeout]
 
-data Command a = CBroadcast (Message a)
-               | CSend NodeId (Message a)
-               | CResetElectionTimeout Int Int
-               | CResetHeartbeatTimeout Int
-               | CLog Builder
-               | CTruncateLog Index
-               | CLogEntries [Entry a]
+-- | Representation of a command to be executed as the result of a state
+-- transition.
+data Command a = CBroadcast (Message a)        -- ^ Broadcast a `Message' to all other nodes
+               | CSend NodeId (Message a)      -- ^ Send a `Message' to some given node
+               | CResetElectionTimeout Int Int -- ^ Reset the election timeout timer to some random value in the given interval
+               | CResetHeartbeatTimeout Int    -- ^ Reset the heartbeat timeout timer to the given time
+               | CLog Builder                  -- ^ Log a message
+               | CTruncateLog Index            -- ^ Truncate the log to given `Index'
+               | CLogEntries [Entry a]         -- ^ Append some entries to the log
   deriving (Show)
 
 instance Arbitrary a => Arbitrary (Command a) where
@@ -324,6 +352,7 @@ instance Arbitrary a => Arbitrary (Command a) where
                  , CLogEntries es
                  ]
 
+-- | Representation of a `RequestVote' request message.
 data RequestVote = RequestVote { rvTerm :: Term
                                , rvCandidateId :: NodeId
                                , rvLastLogIndex :: Index
@@ -339,6 +368,7 @@ instance Arbitrary RequestVote where
                             <*> arbitrary
                             <*> arbitrary
 
+-- | Representation of a `RequestVoteResponse' message.
 data RequestVoteResponse = RequestVoteResponse { rvrTerm :: Term
                                                , rvrVoteGranted :: Bool
                                                }
@@ -350,7 +380,7 @@ instance Arbitrary RequestVoteResponse where
     arbitrary = RequestVoteResponse <$> arbitrary
                                     <*> arbitrary
 
-
+-- | Representation of an `AppendEntries' request message.
 data AppendEntries a = AppendEntries { aeTerm :: Term
                                      , aeLeaderId :: NodeId
                                      , aePrevLogIndex :: Index
@@ -370,7 +400,7 @@ instance Arbitrary a => Arbitrary (AppendEntries a) where
                               <*> arbitrary
                               <*> arbitrary
 
-
+-- | Representation of an `AppendEntriesResponse' message.
 data AppendEntriesResponse = AppendEntriesResponse { aerTerm :: Term
                                                    , aerSuccess :: Bool
                                                    , aerLastIndex :: Index
@@ -384,6 +414,7 @@ instance Arbitrary AppendEntriesResponse where
                                       <*> arbitrary
                                       <*> arbitrary
 
+-- | Enumeration of all message types.
 data Message a = MRequestVote RequestVote
                | MRequestVoteResponse RequestVoteResponse
                | MAppendEntries (AppendEntries a)
@@ -400,6 +431,7 @@ instance Arbitrary a => Arbitrary (Message a) where
         m4 <- MAppendEntriesResponse `fmap` arbitrary
         elements [m1, m2, m3, m4]
 
+-- | Class of types which can be wrapped into a `Message'.
 class IsMessage t a where
     toMessage :: t -> Message a
 
