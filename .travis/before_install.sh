8a3b8987c7f5cf56a75f628eaaa15c44a30f9284
Nicolas Trangez <ikke@nicolast.be>
2017-08-28 00:53:51 +0200

    Stash

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,7 +6,7 @@ GRPC_PREFIX=$HOME/.local
 mkdir -p $HOME/.local/bin
 
 # Install Stack
-travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
+curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
 chmod a+x $HOME/.local/bin/stack
 
 # Install GRPC
--- .travis/install.sh
+++ .travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xue
 
-travis_wait stack -j 2 --no-terminal setup
-travis_wait stack -j 2 --no-terminal build --test --haddock --only-snapshot
-travis_wait stack -j 2 --no-terminal build --haddock grpc-haskell
+stack -j 2 --no-terminal setup
+stack -j 2 --no-terminal build --test --haddock --only-snapshot
+stack -j 2 --no-terminal build --haddock grpc-haskell
