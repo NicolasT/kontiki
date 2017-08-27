b3809bc7fce07ae59975a61ffb4b0c807fe1c11a
Nicolas Trangez <ikke@nicolast.be>
2017-08-28 00:03:26 +0200

    Stash

---
+++ .travis/before_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -xue
+
+GRPC_TAG=v1.3.9
+GRPC_PREFIX=$HOME/.local
+
+mkdir -p $HOME/.local/bin
+
+# Install Stack
+travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $HOME/.local/bin '*/stack'
+chmod a+x $HOME/.local/bin/stack
+
+# Install GRPC
+if ! [ -f ${GRPC_PREFIX}/include/grpc/grpc.h ]; then
+        pushd /tmp
+        git clone --recurse-submodules --branch ${GRPC_TAG} https://github.com/grpc/grpc.git
+        pushd grpc
+        make -j2 prefix=${GRPC_PREFIX} install
+        popd
+        popd
+fi
+
+# Configure Stack
+cat >> ~/.stack/config.yaml << EOF
+extra-include-dirs:
+- ${GRPC_PREFIX}/include
+extra-lib-dirs:
+- ${GRPC_PREFIX}/lib
+EOF
